b92e9a4add617fb8e786de484077e7aa96f847b2
Kaustubh Patil <kaustubhpatil6729@gmail.com>
2025-02-16 07:42:57 +0530

    fund feasibility api

--- src/main/python/api/routes.py
+++ src/main/python/api/routes.py
@@ -17,7 +17,8 @@ SECRET_KEY = "mysecretkey"
 ALGORITHM = "HS256"
 
 # MongoDB Setup
-client = MongoClient("mongodb+srv://saloneevelonde:FczHrpkg8u6qOeMv@cluster0.z6ubj.mongodb.net/")
+client = MongoClient("mongodb://localhost:27017")
+#client = MongoClient("mongodb+srv://saloneevelonde:FczHrpkg8u6qOeMv@cluster0.z6ubj.mongodb.net/", tls=True, tlsAllowInvalidCertificates=True, ssl_cert_reqs=ssl.CERT_NONE)
 db = client["invest_nexus"]
 users_collection = db["users"]
 
